a5849f1ba0e6500d99d98dd6bdb4bf8ba6b649e4
Chanyu Kim <chanyukim15@gmail.com>
2024-10-21 22:24:11 -0700

    finished hw3

--- hw3/practice.hs
+++ hw3/practice.hs
@@ -46,3 +46,59 @@ foo = (\x -> (\y -> (\z -> (\t -> map t [x, x+z..y]))))
 -- d)
 bar = foo 1 2 3
 bar :: (Integer -> a) -> [a]
+
+-- Q15
+-- a)
+{- data InstagramUser = Influencer | Normie
+
+-- b)
+lit_collab :: InstagramUser -> InstagramUser -> Bool
+lit_collab Influencer Influencer = True
+lit_collab _ _ = False
+
+-- c)
+data InstagramUser = 
+    Influencer [String] |
+    Normie
+
+-- d)
+is_sponsor :: InstagramUser -> String -> Bool
+is_sponsor Normie _ = False
+is_sponsor (Influencer []) sponsor = False
+is_sponsor (Influencer lst) sponsor
+    | head lst == sponsor = True -- found match
+    | otherwise = is_sponsor (Influencer (tail lst)) sponsor -- check with tails
+-}
+
+-- e)
+data InstagramUser = 
+    Influencer [String] [InstagramUser] |
+    Normie
+
+-- f)
+count_influencers :: InstagramUser -> Integer
+count_influencers Normie = 0
+count_influencers (Influencer _ []) = 0
+count_influencers (Influencer _ lst) =
+    foldl add_follower 0 lst
+    where
+        add_follower :: Integer -> InstagramUser -> Integer
+        add_follower total Normie = total
+        add_follower total (Influencer _ _) = (total + 1)
+
+-- Q16
+-- a)
+ll_contains :: LinkedList -> Integer -> Bool
+ll_contains EmptyList _ = False
+ll_contains (ListNode val next) target = if (val == target) then True else (ll_contains next target)
+
+data LinkedList = EmptyList | ListNode Integer LinkedList deriving Show
+
+-- b)
+ll_insert :: LinkedList -> Integer -> Integer -> LinkedList
+
+-- c)
+ll_insert EmptyList pos node_val = ListNode node_val EmptyList -- if empty (reach end), insert there
+ll_insert (ListNode val next) pos node_val
+    | pos <= 0 = ListNode node_val (ListNode val next) -- if negative or 0, insert in place
+    | otherwise = ListNode val (ll_insert next (pos-1) node_val) -- create new ListNode with next pointing to recursive call
